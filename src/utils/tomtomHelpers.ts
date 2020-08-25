699cdc97e0748da10367363c7f350ed0948aba59
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-08-25 17:40:52 +0300

    add error handling for tomtom helpers

--- src/utils/tomtomHelpers.ts
+++ src/utils/tomtomHelpers.ts
@@ -1,28 +1,36 @@
 import { TomTomPOISearchResponseResult, TomTomOptions } from "./types";
 
 export function buildTomTomRequestUrl(query: string, options: TomTomOptions) {
-  let baseUrl = `https://api.tomtom.com/search/2/search/${query}.json?`;
-  for (const key of Object.keys(options)) {
-    baseUrl += `&${key.toString()}=${options[key].toString()}`;
+  try {
+    let baseUrl = `https://api.tomtom.com/search/2/search/${query}.json?`;
+    for (const key of Object.keys(options)) {
+      baseUrl += `&${key.toString()}=${options[key].toString()}`;
+    }
+    return baseUrl;
+  } catch (error) {
+    throw new Error("Error occured while building request url");
   }
-  return baseUrl;
 }
 
 export function getPlacesFromTomTom(
   query: string,
   options: TomTomOptions
 ): Promise<TomTomPOISearchResponseResult[]> {
-  if (query.length <= 0 || query == undefined)
-    return new Promise((resolve) => resolve([]));
-  const toQuery = encodeURIComponent(query.trim());
+  try {
+    if (query.length <= 0 || query == undefined)
+      return new Promise((resolve) => resolve([]));
+    const toQuery = encodeURIComponent(query.trim());
 
-  const uri = buildTomTomRequestUrl(toQuery, options);
+    const uri = buildTomTomRequestUrl(toQuery, options);
 
-  return fetch(uri)
-    .then((res) => res.json())
-    .then((resJson) => resJson.results || [])
-    .catch((err) => {
-      console.log("err", err);
-      return [];
-    });
+    return fetch(uri)
+      .then((res) => res.json())
+      .then((resJson) => resJson.results || [])
+      .catch((err) => {
+        console.log("err", err);
+        return [];
+      });
+  } catch (error) {
+    throw new Error("Error occured while fetching results");
+  }
 }
